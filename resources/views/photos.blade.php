755fcd9b0751dc21c3781ed93b064bee3bf17ec5
David VanScott <david.vanscott@gmail.com>
2017-07-13 11:33:05 -0400

    Updates for Instagram feed

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -7,7 +7,7 @@
 		<div class="container">
 			<h1>Photos</h1>
 
-			<div class="btn-toolbar mb-4">
+			<div class="btn-toolbar">
 				<div class="btn-group">
 					<a href="{{ route('media.index') }}" class="btn btn-default"><i class="fa fa-arrow-circle-left fa-fw fa-2x"></i></a>
 				</div>
@@ -16,19 +16,39 @@
 				</div>
 			</div>
 
-			<div class="row">
-				@foreach ($media as $m)
-					<div class="col-sm-6">
-						<div class="thumbnail">
+			@foreach ($media as $m)
+				<div class="event {{ ($loop->index % 2 == 1) ? 'reverse' : '' }}">
+					<div class="event__image">
+						@if ($m->type == 'image')
 							<img src="{{ $m->images->standard_resolution->url }}">
+						@endif
 
-							{{-- <div class="caption">
-								<small>{{ $m->caption->text }}</small>
-							</div> --}}
-						</div>
+						@if ($m->type == 'video')
+							<video src="{{ $m->videos->standard_resolution->url }}"
+								   controls
+								   controlsList="nodownload"></video>
+						@endif
 					</div>
-				@endforeach
-			</div>
+					<div class="event__block">
+						<p class="lead">{{ $m->caption->text }}</p>
+
+						<small class="d-flex align-items-center">
+							<i class="fa fa-heart fa-fw mr-1 text-liked"></i>
+							<span class="text-subtle mr-4">{{ $m->likes->count }}</span>
+
+							@if ($m->comments->count > 0)
+								<i class="fa fa-comments fa-fw mr-1 text-subtle"></i>
+								<span class="text-subtle mr-4">{{ $m->comments->count }}</span>
+							@endif
+
+							<i class="fa fa-clock-o fa-fw mr-1 text-subtle"></i>
+							<span class="text-subtle mr-4">{{ Date::createFromTimestamp($m->created_time)->diffForHumans() }}</span>
+
+							<a href="{{ $m->link }}" target="_blank" class="btn btn-sm btn-default">More Info</a>
+						</small>
+					</div>
+				</div>
+			@endforeach
 		</div>
 	</section>
 @endsection
