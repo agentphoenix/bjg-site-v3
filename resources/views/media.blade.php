0d7e31c798db42c74fdc8c895cf68c8fa0472514
David VanScott <david.vanscott@gmail.com>
2017-07-13 10:47:21 -0400

    Some updates for the old media page

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Media')
 
 @section('content')
-	<section>
+	<section v-cloak>
 		<div class="container">
 			<h1>Media</h1>
 
@@ -18,14 +18,91 @@
 			</div>
 
 			<div class="row">
-				<div class="col-sm-6 col-md-4" v-for="video in videos">
-					<div class="thumbnail">
-						<a href="#" @click.prevent="changeMedia(video)">
-							<img :src="video.preview" :alt="video.title">
-						</a>
-						<div class="caption">
-							<h4>@{{ video.title }}</h4>
-							<p v-text="video.summary"></p>
+				<div class="col-md-9">
+					<div class="row">
+						<div class="col-sm-6" v-for="video in filteredVideos">
+							<div class="thumbnail">
+								<a href="#" @click.prevent="changeMedia(video)">
+									<img :src="video.preview" :alt="video.title">
+								</a>
+								<div class="caption">
+									<h4>@{{ video.title }}</h4>
+									{{-- <p v-text="video.summary"></p> --}}
+									<a class="label label-default mr-2"
+									   v-for="category in video.categories"
+									   @click.prevent="searchCategories = category">
+										@{{ category }}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Find Videos</h3>
+						</div>
+						<div class="panel-body">
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Drill"
+										   v-model="searchCategories">
+									Drill
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Tip"
+										   v-model="searchCategories">
+									Tip
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Driver"
+										   v-model="searchCategories">
+									Driver
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Hybrids"
+										   v-model="searchCategories">
+									Hybrids
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Irons"
+										   v-model="searchCategories">
+									Irons
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio"
+										   name="searchCategories"
+										   value="Putting"
+										   v-model="searchCategories">
+									Putting
+								</label>
+							</div>
+						</div>
+
+						<div class="panel-footer">
+							<button type="button" class="btn btn-default btn-block" @click.prevent="searchCategories = ''">Show All Videos</button>
 						</div>
 					</div>
 				</div>
@@ -41,7 +118,20 @@
 
 			data: {
 				media: false,
+				searchCategories: '',
 				videos: {!! $fileList !!}
+			},
+
+			computed: {
+				filteredVideos () {
+					let self = this
+
+					return this.videos.filter(function (video) {
+						let regex = new RegExp(self.searchCategories, 'i')
+
+						return regex.test(video.categories)
+					})
+				}
 			},
 
 			methods: {
