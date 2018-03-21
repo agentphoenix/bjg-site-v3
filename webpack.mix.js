1f1d5d8ad224403ba43462cf5cccb74f7d0f9cbf
David VanScott <david.vanscott@gmail.com>
2018-03-21 15:01:07 -0400

    Add Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
-const { mix } = require('laravel-mix')
+let mix = require('laravel-mix')
+
+require('laravel-mix-tailwind')
 
 mix.sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/responsive.scss', 'public/css')
    .sass('resources/assets/sass/hidpi2x.scss', 'public/css')
    .sass('resources/assets/sass/hidpi3x.scss', 'public/css')
+   .less('resources/assets/less/tailwind.less', 'public/css')
    .options({ processCssUrls: false })
+   .tailwind()
