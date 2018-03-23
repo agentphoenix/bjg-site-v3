99e88d4ab4d92932caabf8c1fdf197f3a842db01
David VanScott <david.vanscott@gmail.com>
2018-03-23 09:13:03 -0400

    Updated booking page

--- resources/views/book-now.blade.php
+++ resources/views/book-now.blade.php
@@ -7,11 +7,13 @@
 		<div class="container">
 			<h1>Book Now</h1>
 
-			<script src="https://www.golfmds.com/ajaxapp_assets/js/ajaxapp.js?v=1519343291646" id="gmd" async="async"
+			<!-- GolfMDs: Begin -->
+			<script src="https://www.golfmds.com/ajaxapp_assets/js/ajaxapp.js?v=1521693657688" id="gmd" async="async"
 					data-colors="44932b"
 					data-style="light"
 					data-user="Brian-Jacobs">
 			</script>
+			<!-- GolfMDs: End -->
 		</div>
 	</section>
 @endsection
